af482f4989edb2140d269e433c79807fd6fea651
alexr225 <69802005+alexr225@users.noreply.github.com>
2020-09-07 14:55:42 +0100

    Create Episode.sql

---
+++ Episode.sql
@@ -0,0 +1,4 @@
+use HESTeam4
+create table episode_dim
+(EPISODEID int identity primary key,EPISODE int,EPIDUR int,EPIEND date,EPISTART date,EPISTAT char(1),EPITYPE char(1),STARTAGE int,ENDAGE int,ACTIVAGE NVARCHAR(3),EPIORDER INT)
+
