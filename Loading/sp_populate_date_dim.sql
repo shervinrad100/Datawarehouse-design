da285d68a130b1aebc8ff08adf7849adad924c33
Shervin Rad <40181828+shervinrad100@users.noreply.github.com>
2020-09-07 12:41:36 +0100

    Update sp_populate_date_dim.sql
    COMPLETED

--- Loading/sp_populate_date_dim.sql
+++ Loading/sp_populate_date_dim.sql
@@ -1,18 +1,38 @@
 
 CREATE OR ALTER PROC sp_populate_date_dim @strt DATE, @end DATE
 AS BEGIN
--- 1
+
+DECLARE @st DATE, @nd DATE
+SET @st = (SELECT MIN([date]) FROM date_dim)
+SET @nd = (SELECT MAX([date]) FROM date_dim)
+
+-- 0
 	IF @strt >= @end 
-		OR (@strt > (SELECT MIN([date]) FROM date_dim)
-			AND  @end < (SELECT MAX([date]) FROM date_dim)
-			)
 	BEGIN
 		PRINT 'ERROR: Start Date > End Date'
 	END;
--- 2
-	IF @strt < (SELECT MIN([date]) FROM date_dim)
-		AND  @end > (SELECT MAX([date]) FROM date_dim)
+
+-- start---------start
+	IF @strt = @st
+		OR @strt = @nd
 	BEGIN
+		PRINT ' start---------start'
+		SET @strt = DATEADD(dd,-1,@st)
+	END;
+
+-- end-------------end
+	IF @end = @nd
+		OR @end = @st
+	BEGIN
+		PRINT 'end-------------end'
+		SET @end = DATEADD(dd,1,@nd)
+	END;
+
+-- strt end |----------------|
+	IF @strt < @st
+			AND  @end < @st
+	BEGIN
+		PRINT 'strt end |----------------|'
 		INSERT INTO date_dim (
 			[DATE] 
 			,SEASON 
@@ -20,27 +40,18 @@ AS BEGIN
 			,[YEAR] 
 		) (
 			SELECT * FROM
-			func_populate_date_dim(@strt,(SELECT MIN([date]) FROM date_dim))
-		)
-		OPTION (MAXRECURSION 0)
-		INSERT INTO date_dim (
-			[DATE] 
-			,SEASON 
-			,MONTHOFYR
-			,[YEAR] 
-		) (
-			SELECT * FROM
-			func_populate_date_dim((SELECT MAX([date]) FROM date_dim),@end)
+			func_populate_date_dim(@strt,DATEADD(dd,-1,@st))
 		)
 		OPTION (MAXRECURSION 0)
 	END;
--- 3
-	IF (@strt < (SELECT MAX([date]) FROM date_dim) 
-		OR  @strt > (SELECT MIN([date]) FROM date_dim)
-		)
-		AND @end > (SELECT MAX([date]) FROM date_dim) 
+
+--   strt |--------end--------|
+	IF @strt < @st
+			AND  (@end < @nd
+				AND @end > @st
+				)
 	BEGIN
-		SET @strt = (SELECT MAX([date]) FROM date_dim) 
+		PRINT 'strt |--------end--------|'
 		INSERT INTO date_dim (
 			[DATE] 
 			,SEASON 
@@ -48,19 +59,140 @@ AS BEGIN
 			,[YEAR] 
 		) (
 			SELECT * FROM
-			func_populate_date_dim(@strt,@end)
+			func_populate_date_dim(@strt,DATEADD(dd,-1,@st))
 		)
 		OPTION (MAXRECURSION 0)
 	END;
+
+--     strt |----------------| end
+	IF @strt < @st
+		AND  @end > @nd
+	BEGIN
+		PRINT 'strt |----------------| end'
+		INSERT INTO date_dim (
+			[DATE] 
+			,SEASON 
+			,MONTHOFYR
+			,[YEAR] 
+		) (
+			SELECT * FROM
+			func_populate_date_dim(@strt,DATEADD(dd,-1,@st))
+		)
+		OPTION (MAXRECURSION 0)
+		INSERT INTO date_dim (
+			[DATE] 
+			,SEASON 
+			,MONTHOFYR
+			,[YEAR] 
+		) (
+			SELECT * FROM
+			func_populate_date_dim(DATEADD(dd,1,@nd),@end)
+		)
+		OPTION (MAXRECURSION 0)
+	END;
+
+--    |------ start---end---------|
+	IF @strt < @nd
+		AND @strt > @st
+		AND @end > @st
+			AND @end < @nd
+	BEGIN
+		PRINT '|------ start---end---------|'
+		PRINT 'ERROR: UNIQUE constraint violated'
+	END;
+--      |------start----------|  end
+	IF (@strt < @nd 
+		AND  @strt > @st
+		)
+		AND @end > @nd 
+	BEGIN
+		PRINT ' |------start----------|  end'
+		SET @strt = @nd 
+		INSERT INTO date_dim (
+			[DATE] 
+			,SEASON 
+			,MONTHOFYR
+			,[YEAR] 
+		) (
+			SELECT * FROM
+			func_populate_date_dim(DATEADD(dd,1,@nd),@end)
+		)
+		OPTION (MAXRECURSION 0)
+	END;
+--  |----------------| start end
+	IF @strt > @nd
+	BEGIN
+		PRINT '|----------------| start end'
+		INSERT INTO date_dim (
+			[DATE] 
+			,SEASON 
+			,MONTHOFYR
+			,[YEAR] 
+		) (
+			SELECT * FROM
+			func_populate_date_dim(DATEADD(dd,1,@nd),@end)
+		)
+		OPTION (MAXRECURSION 0)
+	END;
+
 END;
 GO
 
 
--- episode_dim epiend
+
+-- episode_dim_epiend
+BEGIN TRAN episode_dim_epiend
 DECLARE @s DATE, @e DATE
 SET @s = (SELECT MIN(EPIEND) FROM HES_APC)
 SET @e = (SELECT MAX(EPIEND) FROM HES_APC)
 
 EXEC sp_populate_date_dim 
 	@strt = @s
-	,@end = @e
+	,@end = @e ;
+GO
+COMMIT TRAN episode_dim_epiend
+
+-- episode_dim_epistart
+BEGIN TRAN episode_dim_epistart
+
+DECLARE @s DATE, @e DATE
+SET @s = (SELECT MIN(EPISTART) FROM HES_APC)
+SET @e = (SELECT MAX(EPISTART) FROM HES_APC)
+
+EXEC sp_populate_date_dim 
+	@strt = @s
+	,@end = @e ;
+GO
+
+COMMIT TRAN episode_dim_epistart
+
+-- addmission_dim_ADMIDATE
+BEGIN TRAN addmission_dim_ADMIDATE
+
+DECLARE @s DATE, @e DATE
+SET @s = (SELECT MIN(ADMIDATE) FROM HES_APC)
+SET @e = (SELECT MAX(ADMIDATE) FROM HES_APC)
+
+EXEC sp_populate_date_dim 
+	@strt = @s
+	,@end = @e ;
+GO
+COMMIT TRAN addmission_dim_ADMIDATE
+
+
+
+
+-- elective_dim_ELECDATE do not include 1800-01-01. Sensible start at 2004-12-24
+BEGIN TRAN elective_dim_ELECDATE
+DECLARE @s DATE, @e DATE
+SET @s = '2004-12-24'
+SET @e = (SELECT MAX(ELECDATE) FROM HES_APC)
+
+EXEC sp_populate_date_dim 
+	@strt = @s
+	,@end = @e ;
+GO
+COMMIT TRAN elective_dim_ELECDATE
+
+
+SELECT * FROM date_dim
