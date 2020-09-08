8036129ae2abbe02f353d0054db6de9d2b101dfe
alexr225 <69802005+alexr225@users.noreply.github.com>
2020-09-08 17:13:28 +0100

    Update DiagnosisSQL.sql

--- DiagnosisSQL.sql
+++ DiagnosisSQL.sql
@@ -6,3 +6,10 @@ inner join
 ICD10Codes
 on diagnosis_dim.DIAG_01=ICD10Codes.Code
 order by DIAGNOSISID
+
+--update diagnosis_dim
+--set diagnosis_dim.Description=ICD10Codes.Description
+--from ICD10Codes,diagnosis_dim
+--where ICD10Codes.Code=diagnosis_dim.DIAG_01
+
+select * from diagnosis_dim
