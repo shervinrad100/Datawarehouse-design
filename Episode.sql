27da68b0af0cc6ee753a1bc7f697239db2ef69e8
Shervin Rad <40181828+shervinrad100@users.noreply.github.com>
2020-09-10 09:33:52 +0100

    Update Episode.sql

--- Episode.sql
+++ Episode.sql
@@ -1,4 +1,14 @@
-use HESTeam4
-create table episode_dim
-(EPISODEID int identity primary key,EPISODE int,EPIDUR int,EPIEND date,EPISTART date,EPISTAT char(1),EPITYPE char(1),STARTAGE int,ENDAGE int,ACTIVAGE NVARCHAR(3),EPIORDER INT)
+USE HESTeam4
+CREATE TABLE episode_dim
+(EPISODEID int identity primary key
+ ,EPISODE int
+ ,EPIDUR int
+ ,EPIEND date
+ ,EPISTART date
+ ,EPISTAT char(1)
+ ,EPITYPE char(1)
+ ,STARTAGE int
+ ,ENDAGE int
+ ,ACTIVAGE NVARCHAR(3)
+ ,EPIORDER INT)
 
